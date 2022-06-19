d70b14699473dc31ea94638e0ec0fc86288d9818
jewonyeon <yjw9852@gmail.com>
2022-06-20 00:35:36 +0900

    fix: build error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from '../pages/Main';
-import MyPage from '../pages/MyPage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
