a085885e28acf5b39bfd16a7ec17690ec2aa17f6
zellytozelly <zellytozelly@gmail.com>
2022-06-20 01:44:07 +0900

    Feat: add entire font style

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import reset from 'styled-reset';
 import Router from './routes';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'LeeSeoyun';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   ${reset};
   * {
     box-sizing: border-box;
   }
 
   html, body {
+    font-family: 'LeeSeoyun', Inter, Arial;
     width: 100%;
     height: 100%;
   }
+
+  input, textarea, select, button { 
+    font-family:inherit; 
+  }
+
 `;
 
 function App() {
