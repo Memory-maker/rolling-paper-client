84b5559145384cb7a3a2a99e264b61a7af504d52
Luceta <57941164+Luceta@users.noreply.github.com>
2022-06-19 20:05:48 +0900

    Update: eslint json

---
+++ src/theme/color.ts
@@ -0,0 +1,13 @@
+type Colors = {
+  MAIN_BG: string;
+  WHITE_COLOR: string;
+  YELLOW_COLOR: string;
+};
+
+const colors: Colors = {
+  MAIN_BG: '#c98128',
+  WHITE_COLOR: '#ffff',
+  YELLOW_COLOR: '#FEE500"
+};
+
+export default colors;
