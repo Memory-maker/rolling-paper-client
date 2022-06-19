1475ef272ee508e7d848164cf42fca0ff530aece
jewonyeon <yjw9852@gmail.com>
2022-06-20 01:48:18 +0900

    feat: Editor layout

---
+++ src/pages/Editor/components/FontFamilyOption.tsx
@@ -0,0 +1,20 @@
+import React, { ChangeEvent } from 'react';
+
+interface FontOptionProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  checked: boolean;
+}
+
+const OPTION_TEXT = '이 글꼴은 어때요?';
+
+function FontFamilyOption({ value, onChange, checked }: FontOptionProps) {
+  return (
+    <label style={{ fontFamily: value, fontSize: '12px' }}>
+      <input type="radio" value={value} name="font-family" onChange={onChange} checked={checked} />
+      {OPTION_TEXT}
+    </label>
+  );
+}
+
+export default FontFamilyOption;
