f0b265f2308926a40f9d2d3ca1af90b5c572554c
kyhyun <k29176@gmail.com>
2022-06-20 02:12:28 +0900

    feat : Add myapge page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,42 @@ const GlobalStyles = createGlobalStyle`
     width: 100%;
     height: 100%;
   }
+
+  @font-face {
+    font-family: 'LeeSeoyun';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+input, textarea, select { font-family:inherit; }
+
+:root {
+  // color
+  --disable-color: #F1F1F1;
+  --background-color: #FFF8EB;;
+  --point-color: #F05A39;
+  --sub-point-color: #FFBA47;
+  --blackground-point-color: #FCEFD6;
+  --b-mid-color: #787878;
+  --b-low-color: #C0C0C0;
+  --point-low-color: #f05a39cc;
+  --background-dark-color: #736D62;
+  --white-color: #ffffff;
+  --black-color: #222222;
+  // card color
+  --card-pink-color: #FFD3D3;
+  --card-blue-color: #D3EAFF;
+  --card-green-color: #DEF9C4;
+  --card-purple-color: #EFCCFF;
+  --card-yellow-color: #FFF5BF;
+  // text color
+  --text-red-color: #FF0000;
+  --text-blue-color : #0085FF;
+  --text-green-color: #7DFF00;
+  --text-purple-color: #AD00FF;
+  --text-yellow-color: #FFD600;
+}
 `;
 
 function App() {
--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,13 +1,43 @@
-import AddButton from './components/addButton';
-import Item from './components/Item';
+import AddButton from './components/AddButton';
+import styled from 'styled-components';
+import Header from './Header';
+import Main from './Main';
 
 function MyPage() {
+  const USER_NAME = '레몬은 아이셔';
+
+  const renderHeaderInfo = () => {
+    return (
+      <>
+        <mark>{USER_NAME}</mark>
+        {' 님의 \n 롤링페이퍼 저장소'}
+      </>
+    );
+  };
+
   return (
-    <>
-      <Item>연습</Item>
-      <AddButton>+</AddButton>
-    </>
+    <MyPageLayout>
+      <HeaderWrapper>
+        <Header>{renderHeaderInfo()}</Header>
+        <Main />
+        <AddButton />
+      </HeaderWrapper>
+    </MyPageLayout>
   );
 }
+
+const MyPageLayout = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const HeaderWrapper = styled.div`
+  position: relative;
+  width: 375px;
+  height: 768px;
+  background: var(--background-color);
+`;
 
 export default MyPage;
