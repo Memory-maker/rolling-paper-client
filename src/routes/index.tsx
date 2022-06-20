b182a08ac461cb346d5bd1e1b95a1fe23ca3a87b
zellytozelly <zellytozelly@gmail.com>
2022-06-20 19:14:30 +0900

    Fix: fix routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import MakeRoll from "../pages/MakeRoll";
 import Home from "../pages/Home";
 import Editor from "../pages/Editor";
 import MyPage from "../pages/MyPage";
@@ -12,7 +11,6 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/editor" element={<Editor />} />
-        <Route path="/create" element={<MakeRoll />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
