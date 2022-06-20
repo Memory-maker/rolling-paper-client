0ebb28e16f48f1edfdec9b59c29386bdb49998d5
kyhyun <k29176@gmail.com>
2022-06-20 23:05:51 +0900

    fix : Update dropDown component

--- src/pages/MyPage/components/DropDown/index.tsx
+++ src/pages/MyPage/components/DropDown/index.tsx
@@ -1,8 +1,9 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useContext } from "react";
 import styled from "styled-components";
+import { CreateContext } from "../../../../App";
 
 function DropDown() {
-  const [isLock, setIsLock] = useState(false);
+  const toggleContext = useContext(CreateContext);
 
   const handleClickShare = useCallback(() => {
     confirm("공유하기를 진행하시겠습니까?");
@@ -13,7 +14,7 @@ function DropDown() {
   }, []);
 
   const handleClickLock = useCallback(() => {
-    setIsLock((prevState) => !prevState);
+    toggleContext?.setIsLock((prevState) => !prevState);
   }, []);
 
   return (
@@ -25,7 +26,7 @@ function DropDown() {
         삭제하기
       </button>
       <button type="button" onClick={handleClickLock}>
-        {isLock ? "모두 보기" : "나만 보기"}
+        {toggleContext?.isLock ? "모두 보기" : "나만 보기"}
       </button>
     </StyledDropDownWrap>
   );
@@ -35,11 +36,11 @@ export default DropDown;
 
 const StyledDropDownWrap = styled.div`
   position: absolute;
+  right: 18px;
+  top: 230px;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  right: 17px;
-  top: 225px;
   width: 140px;
   height: 100px;
   background: #fff;
