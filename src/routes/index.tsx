6e61e9d0d51c659aec08ef0456573019d9fc3e16
Luceta <57941164+Luceta@users.noreply.github.com>
2022-06-20 17:44:03 +0900

    Add: sending share page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import MakeRoll from "../pages/MakeRoll";
 import Home from "../pages/Home";
 import Editor from "../pages/Editor";
 import MyPage from "../pages/MyPage";
+import Sending from "../pages/Sending";
 
 function Router() {
   return (
@@ -14,6 +15,7 @@ function Router() {
         <Route path="/editor" element={<Editor />} />
         <Route path="/create" element={<MakeRoll />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/Sending" element={<Sending />} />
       </Routes>
     </BrowserRouter>
   );
--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,33 +1,35 @@
-// type Colors = {
-//   MAIN_BG: string;
-//   POINT_COLOR: string;
-//   SUBPOINT_COLOR: string;
-//   POINT_ROW_COLOR: string;
-//   GRAY_MID_COLOR: string;
-//   GRAY_LOW_COLOR: string;
-//   DARK_BG_COLOR: string;
+type Colors = {
+  MAIN_BG: string;
+  POINT_COLOR: string;
+  SUBPOINT_COLOR: string;
+  POINT_ROW_COLOR: string;
+  GRAY_MID_COLOR: string;
+  GRAY_LOW_COLOR: string;
+  DARK_BG_COLOR: string;
 
-//   KAKAO_COLOR: string;
-//   WHITE_COLOR: string;
-// };
+  KAKAO_COLOR: string;
+  WHITE_COLOR: string;
+};
 
-// export interface CardColor {
-//   CARD_RED: string;
-//   CARD_BLUE: string;
-//   CARD_GREEN: string;
-//   CARD_PURPLE: string;
-//   CARD_YELLOW: string;
-// }
+export interface CardColor {
+  CARD_RED: string;
+  CARD_BLUE: string;
+  CARD_GREEN: string;
+  CARD_PURPLE: string;
+  CARD_YELLOW: string;
+  CARD_BEIGE: string;
+  CARD_WHITE: string;
+}
 
-// export interface TextColor {
-//   TEXT_RED_COLOR: string;
-//   TEXT_BLUE_COLOR: string;
-//   TEXT_GREEN_COLOR: string;
-//   TEXT_PURPLE_COLOR: string;
-//   TEXT_YELLOW_COLOR: string;
-// }
+export interface TextColor {
+  TEXT_RED_COLOR: string;
+  TEXT_BLUE_COLOR: string;
+  TEXT_GREEN_COLOR: string;
+  TEXT_PURPLE_COLOR: string;
+  TEXT_YELLOW_COLOR: string;
+}
 
-const cardColor = {
+const cardColor: CardColor = {
   CARD_RED: "#FFD3D3",
   CARD_BLUE: "#D3EAFF",
   CARD_GREEN: "#DEF9C4",
@@ -35,17 +37,17 @@ const cardColor = {
   CARD_YELLOW: "#FFF5BF",
   CARD_BEIGE: "#FFDEBF",
   CARD_WHITE: "#FFFFFF",
-} as { [id: string]: string };
+};
 
-const textColor = {
+const textColor: TextColor = {
   TEXT_RED_COLOR: "#FF0000",
   TEXT_BLUE_COLOR: "#0085FF",
   TEXT_GREEN_COLOR: "#7DFF00",
   TEXT_PURPLE_COLOR: "#AD00FF",
   TEXT_YELLOW_COLOR: "#FFD600",
-} as { [id: string]: string };
+};
 
-const colors = {
+const colors: Colors = {
   MAIN_BG: "#FFF8EB",
   POINT_COLOR: "#F05A39",
   SUBPOINT_COLOR: "#FFBA47",
@@ -56,6 +58,6 @@ const colors = {
 
   KAKAO_COLOR: "#FEE500",
   WHITE_COLOR: "#ffff",
-} as { [id: string]: string };
+};
 
 export { colors, cardColor, textColor };
